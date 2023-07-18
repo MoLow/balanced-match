80d21efec0e5ddfdcce5bdd0657e1a0799fa1e43
Moshe Atlow <moshe@atlow.co.il>
2023-07-18 22:01:32 +0300

    use primordials

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict'
+const {
+  StringPrototypeSlice,
+  StringPrototypeIndexOf,
+  StringPrototypeMatch,
+  ArrayPrototypePush,
+  ArrayPrototypePop
+} = require('node-primordials')
 
 /**
  * @param {string | RegExp} a
@@ -15,9 +22,9 @@ function balanced (a, b, str) {
     r && {
       start: r[0],
       end: r[1],
-      pre: str.slice(0, r[0]),
-      body: str.slice(r[0] + a.length, r[1]),
-      post: str.slice(r[1] + b.length)
+      pre: StringPrototypeSlice(str, 0, r[0]),
+      body: StringPrototypeSlice(str, r[0] + a.length, r[1]),
+      post: StringPrototypeSlice(str, r[1] + b.length)
     }
   )
 }
@@ -27,7 +34,7 @@ function balanced (a, b, str) {
  * @param {string} str
  */
 function maybeMatch (reg, str) {
-  const m = str.match(reg)
+  const m = StringPrototypeMatch(str, reg)
   return m ? m[0] : null
 }
 
@@ -40,8 +47,8 @@ balanced.range = range
  */
 function range (a, b, str) {
   let begs, beg, left, right, result
-  let ai = str.indexOf(a)
-  let bi = str.indexOf(b, ai + 1)
+  let ai = StringPrototypeIndexOf(str, a)
+  let bi = StringPrototypeIndexOf(str, b, ai + 1)
   let i = ai
 
   if (ai >= 0 && bi > 0) {
@@ -53,18 +60,18 @@ function range (a, b, str) {
 
     while (i >= 0 && !result) {
       if (i === ai) {
-        begs.push(i)
-        ai = str.indexOf(a, i + 1)
+        ArrayPrototypePush(begs, i)
+        ai = StringPrototypeIndexOf(str, a, i + 1)
       } else if (begs.length === 1) {
-        result = [begs.pop(), bi]
+        result = [ArrayPrototypePop(begs), bi]
       } else {
-        beg = begs.pop()
+        beg = ArrayPrototypePop(begs)
         if (beg < left) {
           left = beg
           right = bi
         }
 
-        bi = str.indexOf(b, i + 1)
+        bi = StringPrototypeIndexOf(str, b, i + 1)
       }
 
       i = ai < bi && ai >= 0 ? ai : bi
